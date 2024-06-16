9ee1e53661246e9bb8f75c323088b550b815ed04
“[Asoon-D-Roger]” <[asoon1067@gmail.com]>
2024-06-16 20:36:16 +0530

    change

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -17,8 +17,8 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
-// import WorkSliderBtns from "@/components/WorkSliderbtns";
-import WorkSliderbtns from "@/components/WorkSliderntns";
+import WorkSliderntns from "@/components/WorkSliderntns";
+// import WorkSliderbtns from "@/components/WorkSliderntns";
 
 const projects = [
   {
@@ -206,7 +206,7 @@ const Work = () => {
                 );
               })}
               {/* slider buttons */}
-              <WorkSliderbtns
+              <WorkSliderntns
                 containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between 
               xl:w-max xl:justify-none"
                 btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px]
